6538c46b63ed15cdc79e2822edee2dc676425161
Lito Arias <lito.arias.cervero@gmail.com>
2018-10-06 22:29:18 +0200

    refactor network

--- NetworkClient.xctemplate/Networking/___FILEBASENAME___.swift
+++ NetworkClient.xctemplate/Networking/___FILEBASENAME___.swift
@@ -9,41 +9,36 @@ enum Result<T> {
 
 class ___FILEBASENAMEASIDENTIFIER___ {
     
-    static func get<K: Codable>(url: URL, completion: @escaping (Result<K>) -> Void) {
+    static func get<K: Codable>(url: URL, parameters: [String: Any]? = [:], completion: @escaping (Result<K>) -> Void) {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        let session = URLSession(configuration: URLSessionConfiguration.default)
         
-        let task = session.dataTask(with: request) { (responseData, response, responseError) in
-            if let error = responseError {
-                completion(.failure(error))
-            } else if let jsonData = responseData {
-                let decoder = JSONDecoder()
-                do {
-                    let objects = try decoder.decode(K.self, from: jsonData)
-                    let result: Result<K> = Result.success(objects)
-                    completion(result)
-                } catch {
-                    completion(.failure(error))
-                }
-            }
-        }
+        var urlComp = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        parameters?.forEach({
+            urlComp?.queryItems?.append(URLQueryItem(name: $0.key, value: $0.value as? String))
+        })
         
-        task.resume()
+        NetworkClient().task(request: request, completion: completion).resume()
     }
     
     
     static func post<K: Codable>(url: URL, parameters: [String: Any], completion: @escaping (Result<K>) -> Void) {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
         guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
             return
         }
         request.httpBody = httpBody
         request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
         
-        let task = session.dataTask(with: request) { (responseData, response, responseError) in
+        NetworkClient().task(request: request, completion: completion).resume()
+    }
+    
+    
+    fileprivate func task<K: Codable>(request: URLRequest, completion: @escaping (Result<K>) -> Void) -> URLSessionDataTask {
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        return session.dataTask(with: request) { (responseData, response, responseError) in
             if let error = responseError {
                 completion(.failure(error))
             } else if let jsonData = responseData {
@@ -57,8 +52,6 @@ class ___FILEBASENAMEASIDENTIFIER___ {
                 }
             }
         }
-        
-        task.resume()
     }
     
 }
